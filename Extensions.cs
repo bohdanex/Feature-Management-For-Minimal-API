15b59de00ae75179eb21c476e219fdd8abe3b907
bohdan mykhats <bodiamykhats@gmail.com>
2024-08-05 22:51:40 +0300

    Add project files.

---
+++ Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Bodatero.FeatureManagementMinimalAPI.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bodatero.FeatureManagementMinimalAPI
+{
+    public static class FeatureManagementMinimalApiExtensions
+    {
+        public static IServiceCollection AddFeatureGateForMinimalAPI(this IServiceCollection services)
+        {
+            services.AddSingleton<FeatureGateForMinimalApiMiddleware>();
+            return services;
+        }
+
+        public static IApplicationBuilder UseMinimalApiFeatureGate(this IApplicationBuilder appBuilder)
+        {
+            appBuilder.UseMiddleware<FeatureGateForMinimalApiMiddleware>();
+            return appBuilder;
+        }
+    }
+}
---
+++ Middlewares/FeatureGateForMinimalApiMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bodatero.FeatureManagementMinimalAPI.Middlewares
+{
+    internal class FeatureGateForMinimalApiMiddleware : IMiddleware
+    {
+        private readonly IFeatureManager _featureManager;
+        private readonly ILogger<FeatureGateForMinimalApiMiddleware> _logger;
+
+        public FeatureGateForMinimalApiMiddleware(IFeatureManager featureManager, ILogger<FeatureGateForMinimalApiMiddleware> logger)
+        {
+            _featureManager = featureManager;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            Endpoint? endpoint = context.Features.Get<IEndpointFeature>()?.Endpoint;
+            if (endpoint != null)
+            {
+                FeatureGateAttribute? featureGateAttribute = endpoint.Metadata.GetMetadata<FeatureGateAttribute>();
+                if (featureGateAttribute is not null)
+                {
+                    bool[] results = await Task.WhenAll(featureGateAttribute.Features.Select(f => _featureManager.IsEnabledAsync(f)));
+                    if (!results.All(b => b))
+                    {
+                        _logger.LogInformation("Feature gate check failed for {Endpoint}. Required features: {Features}",
+                            endpoint.DisplayName, string.Join(", ", featureGateAttribute.Features));
+
+                        context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                        await context.Response.WriteAsync("Feature not enabled.");
+                        return;
+                    }
+                }
+            }
+
+            await next(context);
+        }
+    }
+}
